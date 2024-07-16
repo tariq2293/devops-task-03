7280892d27cbd51708a8893d9db5578cdb09727c
tariq2293 <tariq@provility.com>
2024-07-16 15:07:16 +0530

    Upgrade logback library and add image url

---
+++ feature_branch.sh
@@ -0,0 +1,12 @@
+git checkout -b feature/changes
+
+git pull
+
+
+# check and commit changes
+git diff
+git add .
+git commit -m "Upgrade logback library and add image url"
+
+# push your changes to remote
+git push
